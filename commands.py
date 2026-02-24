c1275f46b0ec394169c6cc08c62ecf9a0d5159a9
akcoder15 <adhyanshamit2019@gmail.com>
2026-02-24 13:20:52 +0400

    added get_deadline

--- commands.py
+++ commands.py
@@ -1,5 +1,15 @@
 import sqlite3
-from date import get_deadline #Accept and format deadline date input
+from datetime import datetime
+
+def get_deadline(): #Accept and format deadline date input
+    while True:
+        user_input = input("Enter deadline (YYYY-MM-DD HH:MM): ")
+        try:
+            # Validate format: If it fails, it jumps to 'except'
+            valid_dt = datetime.strptime(user_input, "%Y-%m-%d %H:%M")
+            return valid_dt.strftime("%Y-%m-%d %H:%M:%S")
+        except ValueError:
+            print("Invalid format! Please use YYYY-MM-DD HH:MM (e.g., 2026-02-25 18:00)")
 
 con = sqlite3.connect("tasks.db") # Connecting to database
 cur = con.cursor() # Creating a cursor to execute SQL commands
